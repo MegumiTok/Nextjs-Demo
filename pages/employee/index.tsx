a637df4b585c895b86921c16eff7b567b62b1cc9
sleepyshowdy <hojo.miku@gmail.com>
2023-02-07 06:22:40 +1100

    ボタン追加✅

--- pages/employee/index.tsx
+++ pages/employee/index.tsx
@@ -1,5 +1,6 @@
 import Layout from "../../components/layout";
 import Head from "next/head";
+import { FiUserPlus } from "react-icons/fi";
 export default function Employee() {
   return (
     <Layout>
@@ -7,11 +8,26 @@ export default function Employee() {
         <Head>
           <title>Employee Management</title>
         </Head>
-        <header>
+        <main className="py-5 ">
           <h1 className="text-xl md:text-5xl font-bold text-center py-10">
             Employee Management
           </h1>
-        </header>
+
+          <div className="container mx-auto flex justify-between py-5 border-b">
+            <div className="left flex gap-3">
+              <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-indigo-300 hover:border-indigo-300  hover:text-gray-800">
+                Add Employees{" "}
+                <span className="px-1">
+                  <FiUserPlus size={23}></FiUserPlus>
+                </span>
+              </button>
+            </div>
+
+            {/* collapsible form */}
+
+            {/* table */}
+          </div>
+        </main>
       </section>
     </Layout>
   );
