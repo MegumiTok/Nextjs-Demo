290e9f3fb789624cfaace11e8867bff8b2708b4a
sleepyshowdy <hojo.miku@gmail.com>
2023-02-07 13:21:27 +1100

    Database Connected

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectMongo from "../../database/conn";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -9,5 +10,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  connectMongo();
   res.status(200).json({ name: "John Doe" });
 }
