f64e788ec8be6801bd1b41200dc9b7e52f9144a3
sleepyshowdy <hojo.miku@gmail.com>
2023-02-09 20:40:58 +1100

    Querying with dynamic keys

--- database/controller.ts
+++ database/controller.ts
@@ -15,6 +15,21 @@ export async function getUsers(req: NextApiRequest, res: NextApiResponse) {
   }
 }
 
+// get:http://localhost:3000/api/users/[userId]
+export async function getUser(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { userId } = req.query;
+
+    if (userId) {
+      const user = await Users.findById(userId);
+      res.status(200).json(user);
+    }
+    res.status(404).json({ error: "User not Selected...!" });
+  } catch (error) {
+    res.status(404).json({ error: "Cannot get the User...!" });
+  }
+}
+
 // post : http://localhost:3000/api/users
 export async function postUser(req: NextApiRequest, res: NextApiResponse) {
   try {
