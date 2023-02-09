aea18ad16d05f94861165a56a95ce69a8eb45c33
sleepyshowdy <hojo.miku@gmail.com>
2023-02-09 20:40:58 +1100

    Querying with dynamic keys

---
+++ pages/api/users/[userId].ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectMongo from "../../../database/conn";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ResponseFuncs } from "@/lib/types";
+import { getUser, postUser, putUser, deleteUser } from "@/database/controller";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  connectMongo().catch(() =>
+    res.status(405).json({ error: "Error in the Connection" })
+  );
+
+  const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
+
+  switch (method) {
+    case "GET":
+      getUser(req, res);
+      break;
+
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
