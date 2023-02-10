a65c4ada802b79b1a9af38bbb5d16907b7b30db0
sleepyshowdy <hojo.miku@gmail.com>
2023-02-10 19:51:40 +1100

    delete user

--- components/table.tsx
+++ components/table.tsx
@@ -3,7 +3,11 @@ import { BiEdit, BiTrashAlt } from "react-icons/bi";
 import { getUsers } from "@/lib/helper";
 import { useQuery } from "react-query";
 import { useAppSelector, useAppDispatch } from "@/redux/store";
-import { toggleChangeAction, updateAction } from "@/redux/reducer";
+import {
+  toggleChangeAction,
+  updateAction,
+  deleteAction,
+} from "@/redux/reducer";
 
 export default function Table() {
   // console.log(getUser());
@@ -64,6 +68,12 @@ function Tr({ _id, name, avatar, email, salary, date, status }) {
       dispatch(updateAction(_id));
     }
   };
+
+  const onDelete = () => {
+    if (!visible) {
+      dispatch(deleteAction(_id));
+    }
+  };
   return (
     <tr className="bg-gray-50 text-center">
       <td className="px-16 py-2 flex flex-row items-center">
@@ -101,7 +111,7 @@ function Tr({ _id, name, avatar, email, salary, date, status }) {
         <button className="cursor" onClick={onUpdate}>
           <BiEdit size={25} color={"rgb(34,197,94"}></BiEdit>
         </button>
-        <button>
+        <button className="cursor" onClick={onDelete}>
           <BiTrashAlt size={25} color={"rgb(244,63,94"}></BiTrashAlt>
         </button>
       </td>
